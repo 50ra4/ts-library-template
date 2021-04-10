26bd7620175c349d18a30e250d43f002a6cf7503
s.igarashi <50ra4.dev.dev@gmail.com>
2021-04-10 14:09:05 +0900

    add: sample source

---
+++ src/calc.ts
@@ -0,0 +1,19 @@
+/**
+ * add
+ * @param n1 number1
+ * @param n2 number2
+ * @returns number1 + number2
+ */
+export const add = (n1: number, n2: number): number => n1 + n2;
+/**
+ * inc
+ * @param n number
+ * @returns number + 1
+ */
+export const inc = (n: number) => n + 1;
+/**
+ * dec
+ * @param n number
+ * @returns number - 1
+ */
+export const dec = (n: number) => n - 1;
